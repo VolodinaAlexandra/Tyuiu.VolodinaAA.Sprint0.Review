ad20ce1b247a95cf5ec8e0e4d65704ce04e55e97
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-10-03 08:38:32 +0300

    Add project files.

---
+++ Tyuiu.VolodinaAA.Sprint0.TaskReview.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VolodinaAA.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VolodinaAA.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bcc83b98-7d4c-48d2-bf3f-a8a2e0b68972")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VolodinaAA.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolodinaAA.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.VolodinaAA.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт#0 |Выполнила: Володина А.А.|ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#0                                                                *");
+            Console.WriteLine("* Тема: Создание итогового решения по проекту                             *");
+            Console.WriteLine("* Задание #Review                                                         *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнила: Володина Александра Александровна | ПКТб-23-1                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая вычисляет (x+y+z)*5        *");
+            Console.WriteLine("* и выводит результат на экран.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* x=1; y=2; z=3                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (x+y+z)*5 = " + DataService.Calc(1, 2, 3) + "                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
